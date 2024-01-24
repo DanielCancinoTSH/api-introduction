a7effc38b079b53019c477dec3ef5e10090d7286
DanielCancinoTSH <138708926+DanielCancinoTSH@users.noreply.github.com>
2024-01-24 16:06:09 -0500

    Added the necessary files

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button');
+const fact = document.querySelector('.fact')
+
+function fetchData() {
+    const apiUrl = 'https://catfact.ninja/fact';
+  
+   
+  }
+
+  button.addEventListener('click', fetchData);
